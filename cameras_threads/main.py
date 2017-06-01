fa0beab8708f3fa0bbee3d56cf31a033629fbf8d
mhaut <juanmariohaut@unex.es>
2017-06-01 13:44:21 +0200

    fix typo

--- cameras_threads/main.py
+++ cameras_threads/main.py
@@ -1,13 +1,14 @@
 import numpy as np
 import time
 from PyQt4 import QtCore
-from proba import Manyfoxcam
+from foxcamthreads import Manyfoxcam
 import cv2
 import matplotlib.pyplot as plt
 
 class Camera(object):
 	urls = []
-	#urls.append("IP:PORT/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=X&pwd=Y&.mjpg")
+	#urls.append("http://IP:PORT/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=X&pwd=Y&.mjpg")
+
 	alltheads = [None] * len(urls)
 	for url, i in zip(urls, range(len(urls))):
 		alltheads[i] = Manyfoxcam(url)
@@ -24,10 +25,9 @@ class Camera(object):
 		for i in range(len(urls)):
 			if alltheads[i].frame is not None:
 				fcam.append(cv2.cvtColor(alltheads[i].frame, cv2.COLOR_BGR2RGB))
-		if len(fcam) == 2:
+		if len(fcam) == len(urls):
 			plt.imshow(np.hstack((fcam[0],fcam[1])))
-			plt.pause(0.1)
+			plt.pause(0.03)
 
-
-					
+############   MAIN   #################
 a = Camera()
